520e46a8d8bcf839c7bef5a2cc9dca4f8e4815f7
calledude <22471295+calledude@users.noreply.github.com>
2024-03-06 19:26:29 +0100

    AOT Use minimal hosting model instead of Startup/Program combo

--- SpotifyVolumeExtensionApi/Program.cs
+++ SpotifyVolumeExtensionApi/Program.cs
@@ -1,21 +1,43 @@
-using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace SpotifyVolumeExtensionApi;
 
 public static class Program
 {
-	public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+	public static void Main(string[] args)
+	{
+		var clientId = Environment.GetEnvironmentVariable("CLIENT_ID");
+		var clientSecret = Environment.GetEnvironmentVariable("CLIENT_SECRET");
 
-	public static IWebHostBuilder CreateHostBuilder(string[] args) => WebHost.CreateDefaultBuilder(args)
-			.ConfigureLogging(logger =>
-			{
-				logger
-					.SetMinimumLevel(LogLevel.Debug)
-					.AddFilter("Microsoft", LogLevel.Information)
-					.AddFilter("System", LogLevel.Information);
-			})
-			.UseStartup<Startup>();
+		if (string.IsNullOrWhiteSpace(clientId) || string.IsNullOrWhiteSpace(clientSecret))
+			return;
+
+		var builder = WebApplication.CreateSlimBuilder(args);
+		builder.WebHost.UseKestrelHttpsConfiguration();
+
+		builder.Logging
+			.SetMinimumLevel(LogLevel.Debug)
+			.AddFilter("Microsoft", LogLevel.Information)
+			.AddFilter("System", LogLevel.Information);
+
+		builder.Services.AddHttpClient();
+
+		var app = builder.Build();
+
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseDeveloperExceptionPage();
+		}
+
+		app.UseHttpsRedirection();
+
+		app.MapSpotifyAuthenticationRoutes(clientId, clientSecret);
+
+		app.Run();
+	}
 }
---
+++ SpotifyVolumeExtensionApi/SpotifyAuthenticationRouteExtensions.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace SpotifyVolumeExtensionApi;
+
+public static class SpotifyAuthenticationRouteExtensions
+{
+	private const string _redirectUri = "https://svetokenswap.azurewebsites.net";
+
+	public static IEndpointRouteBuilder MapSpotifyAuthenticationRoutes(this IEndpointRouteBuilder builder, string clientId, string clientSecret)
+	{
+		return builder
+			.MapSpotifyAuthorizationCallbackEndpoint()
+			.MapAuthorizeEndpoint(clientId)
+			.MapTokenSwapEndpoints(clientId, clientSecret);
+	}
+
+	private static IEndpointRouteBuilder MapSpotifyAuthorizationCallbackEndpoint(this IEndpointRouteBuilder builder)
+	{
+		// This is the endpoint that will be hit after '/authorize' completes its Redirect to spotify, which later on will return here
+		// See: _redirectUri
+		builder.MapGet("/", ([FromQuery] string code, [FromQuery] string? state) =>
+		{
+			var query = new Dictionary<string, string?>()
+			{
+				{ "code", code },
+				{ "state", state }
+			};
+
+			var queryString = QueryHelpers.AddQueryString("http://localhost:4002/auth", query);
+			return Results.Redirect(queryString);
+		}).DisableAntiforgery();
+
+		return builder;
+	}
+
+	private static IEndpointRouteBuilder MapAuthorizeEndpoint(this IEndpointRouteBuilder builder, string clientId)
+	{
+		builder.MapGet("/authorize", (
+			[FromQuery] string response_type,
+			[FromQuery] string? state,
+			[FromQuery] string scope,
+			[FromQuery] string? show_dialog) =>
+		{
+			var query = new Dictionary<string, string?>()
+			{
+				{ "client_id", clientId },
+				{ "redirect_uri", _redirectUri },
+				{ "response_type", response_type },
+				{ "scope", scope },
+				{ "state", state },
+				{ "show_dialog", show_dialog }
+			};
+
+			var queryString = QueryHelpers.AddQueryString("https://accounts.spotify.com/authorize/", query);
+			return Results.Redirect(queryString);
+		}).DisableAntiforgery();
+
+		return builder;
+	}
+
+	private static IEndpointRouteBuilder MapTokenSwapEndpoints(this IEndpointRouteBuilder builder, string clientId, string clientSecret)
+	{
+		builder.MapPost("/swap", async ([RequiredFromForm] string code, IHttpClientFactory httpClientFactory) =>
+		{
+			var tokenResponse = await GetToken(httpClientFactory, "authorization_code", code: code);
+			return Results.Content(tokenResponse, MediaTypeNames.Application.Json);
+		}).DisableAntiforgery();
+
+		builder.MapPost("/refresh", async ([RequiredFromForm] string refresh_token, IHttpClientFactory httpClientFactory) =>
+		{
+			var tokenResponse = await GetToken(httpClientFactory, "refresh_token", refreshToken: refresh_token);
+			return Results.Content(tokenResponse, MediaTypeNames.Application.Json);
+		}).DisableAntiforgery();
+
+		return builder;
+
+		async Task<string> GetToken(IHttpClientFactory httpClientFactory, string grantType, string code = "", string refreshToken = "")
+		{
+			var client = httpClientFactory.CreateClient();
+
+			var query = new Dictionary<string, string>()
+			{
+				{ "grant_type", grantType },
+				{ "redirect_uri", _redirectUri },
+				{ "code", code },
+				{ "client_id", clientId },
+				{ "client_secret", clientSecret },
+				{ "refresh_token", refreshToken }
+			};
+
+			var content = new FormUrlEncodedContent(query);
+			var response = await client.PostAsync("https://accounts.spotify.com/api/token", content);
+
+			return await response.Content.ReadAsStringAsync();
+		}
+	}
+}
